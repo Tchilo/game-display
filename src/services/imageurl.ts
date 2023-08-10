26a18ed683af35c9242c9af1d8fcc50e27ec1aa7
Tchilo <tchiloross@gmail.com>
2023-08-10 22:45:08 +0200

    Handle games without images

--- src/services/imageurl.ts
+++ src/services/imageurl.ts
@@ -1,6 +1,7 @@
+import noImage from '../assets/noImage.jpg'
 const getCroppedImage = (url: string) => {
     const target = 'media/'
-    if(!url) return ''
+    if(!url) return noImage
     const index = url.indexOf(target) + target.length
     return url.slice(0,index)+ 'crop/600/400/' + url.slice(index)
 
