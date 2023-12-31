366c71142fe7a5b3854040dee430d7987b521346
Tchilo <tchiloross@gmail.com>
2023-12-31 16:56:00 +0200

    Fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react"
-import logo from '../assets/logo.svg'
+import logo from '../assets/lambda-logo.png'
 import ColorModeSwitch from "./ColorModeSwitch"
 import SearchInput from "./SearchInput"
+import { Link } from "react-router-dom"
 
 
 function Navbar() {
     return (
-        <HStack padding='10px'>
-        <Image src={logo} boxSize='60px' />
+      <HStack padding='10px'>
+        <Link to='/'>
+        <Image src={logo} boxSize='55px' objectFit='cover' />
+        </Link>
         <SearchInput/>
           <ColorModeSwitch/>
     </HStack>
